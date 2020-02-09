93d6556c6e25e89c8ff40b50549ea679a2fe2a75
Gustavo Henrique <ghdpreto@gmail.com>
2020-02-09 20:52:00 -0300

    Paginas dinamicamente alimentadas, utilizando nunjucks

--- desafios/modulo_2/foodfy/server.js
+++ desafios/modulo_2/foodfy/server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const nunjucks = require("nunjucks")
 
-const server = express()
+const data = require("./data")
 
+const server = express()
 
 server.use(express.static("public"))
 
@@ -19,5 +20,17 @@ server.listen(5001, function(){
 })
 
 server.get("/", function (req, res) {
-    return res.render("index")
+    return res.render("index", { items: data})
+})
+
+server.get("/recites-info", function (req, res) {
+    return res.render("recites-info")
+})
+
+server.get("/about", function (req, res) {
+    return res.render("about")
+})
+
+server.get("/food-recipes", function (req, res) {
+    return res.render("food-recipes", { items: data })
 })
